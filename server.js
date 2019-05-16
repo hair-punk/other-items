22c69869079046f0f50971087cf969aa24dbf423
jvc5546 <jvc5546@gmail.com>
2019-05-16 13:47:45 -0500

    Added use of morgan for the server

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const morgan = require('morgan');
 var app = express();
 var port = 3003;
-
+app.use(morgan('tiny'));
 app.use(express.static('public'));
 
 app.listen(port,(err) => {
