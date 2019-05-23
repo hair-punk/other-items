a798f061a7997d873bce8f0b1cf0b581d274551d
jvc5546 <jvc5546@gmail.com>
2019-05-23 17:47:46 -0500

    Removed unnecessary comments

--- database/seedFunction.js
+++ database/seedFunction.js
@@ -12,34 +12,6 @@ const s3 = new S3({
 });
 const Bucket = 'testjvc5546';
 
-// module.exports.start = function(cb){
-//   db.Product.deleteMany((err) => {
-//     if(err){
-//       console.log('Error starting seeding process');
-//       cb(err);
-//       return;
-//     }
-//     console.log('Seeding process Started');
-//     var documents = [];
-//     for (var i = 0; i < 100; i++){
-//       var product = new db.Product({
-//         name: faker.commerce.productName(),
-//         owner: faker.company.companyName(),
-//         price: faker.commerce.price(),
-//         imageUrl: `http://lorempixel.com/${Math.floor(Math.random() * 640) + 640}/${Math.floor(Math.random() * 480) + 480}`
-//       });
-//       documents.push(product);
-//     }
-//     db.Product.insertMany(documents, (err) => {
-//       if(err){
-//         cb(err);
-//         return;
-//       }
-//       cb(null);
-//     });
-//   });
-// };
-
 module.exports.start = function(cb) {
   db.Product.deleteMany((err) => {
     if(err){
