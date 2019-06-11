69ffb7bd4db513766c149c7c5fcdebf41b5d5bcc
tuyetvo <ttv5004@gmail.com>
2019-06-11 03:56:00 -0400

    updated data generator script

---
+++ database/dataGenerator.js
@@ -0,0 +1,26 @@
+const faker = require('faker');
+const fs = require('fs');
+
+
+const dataGenerator = (numOfRecords) => {
+  const writeStream = fs.createWriteStream(__dirname + '/data/mockdata.csv', {flag: 'w', defaultEncoding: 'utf8'});
+
+  for (var i = 0; i < numOfRecords; i++) {
+
+    let name = faker.commerce.productName();
+    let owner = faker.company.companyName(0);
+    let price = faker.commerce.price();
+    let imageNumber = faker.random.number({min: 1, max: 250});
+    let image = `https://etsymoreyoumightlikephotos.s3.us-east-2.amazonaws.com/${imageNumber}`
+
+    const dataString = `${name}, ${owner}, ${price}, ${image}\n`;
+
+    writeStream.write(dataString, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    })
+  }
+};
+
+dataGenerator();
