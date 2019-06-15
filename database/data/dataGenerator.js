a2694abacb55db22128f5ee14f6f9cc7c2febee1
tuyetvo <ttv5004@gmail.com>
2019-06-14 21:00:30 -0400

    dataGen added

--- database/data/dataGenerator.js
+++ database/data/dataGenerator.js
@@ -2,18 +2,18 @@ const faker = require('faker');
 const fs = require('fs');
 
 
-const dataGenerator = (numOfRecords) => {
-  const writeStream = fs.createWriteStream(__dirname + '/data/mockdata.csv', {flag: 'w', defaultEncoding: 'utf8'});
+const dataGenerator = async (numOfRecords) => {
+  const writeStream = fs.createWriteStream(__dirname + '/mockdata.csv', {flag: 'w', defaultEncoding: 'utf8'});
 
-  for (var i = 0; i < numOfRecords; i++) {
-
+  for (var i = 1; i < numOfRecords; i++) {
+    let id = i;
     let name = faker.commerce.productName();
     let owner = faker.company.companyName(0);
     let price = faker.commerce.price();
     let imageNumber = faker.random.number({min: 1, max: 250});
     let image = `https://etsymoreyoumightlikephotos.s3.us-east-2.amazonaws.com/${imageNumber}`
 
-    const dataString = `${name}, ${owner}, ${price}, ${image}\n`;
+    const dataString = `${id}, ${name}, ${owner}, ${price}, ${image}\n`;
 
     writeStream.write(dataString, (err) => {
       if (err) {
@@ -23,4 +23,5 @@ const dataGenerator = (numOfRecords) => {
   }
 };
 
-dataGenerator(1e7);
+
+dataGenerator(10000001);
